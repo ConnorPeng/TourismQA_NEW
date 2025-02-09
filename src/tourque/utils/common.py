4945e60ffa4559e12842030e390876aa813094f7
ConnorPeng <pjh_jinghong@outlook.com>
2025-02-08 18:47:57 -0800

    update

---
+++ src/tourque/utils/common.py
@@ -0,0 +1,35 @@
+import json
+import pickle
+from pathlib import Path
+
+def getProjectRootPath() -> Path:
+    # Update the path calculation since the file location has changed
+    return Path(__file__).parent.parent.parent.parent
+
+def create(path) -> None:
+    path = Path(path)
+    path.mkdir(parents = True, exist_ok = True)
+
+def loadJSON(path) -> None:
+    return json.load(open(path, "r", encoding = "utf-8"))
+
+def dumpJSON(data, path, sort_keys = False) -> None:
+    create(Path(path).parent)
+    json.dump(data, open(path, "w", encoding = "utf-8"), indent = 4, ensure_ascii = False, sort_keys = sort_keys)
+
+def dumpPickle(data, path) -> None:
+    create(Path(path).parent)
+    pickle.dump(data, open(path, "wb"))
+
+def update(d, u):
+    for k, v in u.items():
+        if(isinstance(v, dict)):
+            d[k] = update(d[k], v)
+        else:
+            if(isinstance(v, list)):
+                for e in v:
+                    if(e not in d[k]):
+                        d[k].append(e)
+            else:
+                d[k] = v
+    return d 
